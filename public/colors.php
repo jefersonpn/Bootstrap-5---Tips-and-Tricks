e986667fa5d48753795f50ae50be1202faf45b50
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-18 21:20:44 -0400

    Colors

---
+++ public/colors.php
@@ -0,0 +1,42 @@
+<?php include 'header.php'; ?>
+
+<div class="container m-2">
+    <h1>Background Colors</h1>
+    <h3>bg-{color}</h3>
+    <h6>Color:</h6>
+    <ul>
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+        <button type="button" class="btn btn-warning">Warning</button>
+        <button type="button" class="btn btn-info">Info</button>
+        <button type="button" class="btn btn-light">Light</button>
+        <button type="button" class="btn btn-dark">Dark</button>
+
+        <button type="button" class="btn btn-link">Link</button>
+    </ul>
+</div>
+
+<div class="container m-2">
+    <h1>Text Colors</h1>
+    <h3>text-{color}</h3>
+    <h6>Color:</h6>
+    <ul class="inline">
+        <li><h5 class="text-primary">Primary</h5></li>
+        <li><h5 class="text-secondary">Secondary</h5></li>
+        <li><h5 class="text-success">Success</h5></li>
+        <li><h5 class="text-danger">Danger</h5></li>
+        <li><h5 class="text-warning">Warning</h5></li>
+        <li><h5 class="text-info">Info</h5></li>
+        <li><h5 class="text-light">Light</h5></li>
+        <li><h5 class="text-dark">Dark</h5></li>
+
+        <li><h5 type="button" class="btn btn-link">Link</h5></li>
+    </ul>
+
+    <div class="container">
+        <p>Check Theme Color on the documentation of the Bootstrap! for more information.</p>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
