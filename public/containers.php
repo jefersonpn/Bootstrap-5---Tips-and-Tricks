2f39bb4ba559d44b1c3d7d7ddce097ec877c1996
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-18 20:53:47 -0400

    How the containers works

---
+++ public/containers.php
@@ -0,0 +1,32 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-sm">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-md">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-lg">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-xl">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-xxl">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+<div class="container-fluid">
+    <h1>Container</h1>
+    <p>This is a container test, so we can have a look how it works "live". It is the most important tag on css!</p>
+</div>
+
+<?php include 'footer.php'; ?>
