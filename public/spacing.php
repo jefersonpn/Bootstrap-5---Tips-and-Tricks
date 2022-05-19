33088077c4d7189f92eb13e9e9a0638a53cfb07d
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-18 21:55:13 -0400

    Spacing

---
+++ public/spacing.php
@@ -0,0 +1,65 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <h1>Padding</h1>
+    <h3>p{DIR}-{AMT}</h3>
+    <h3><b>DIR</b></h3>
+    <p>t e b s x y</p>
+    <h3><b>AMT</b></h3>
+    <p>0 1 2 3 4 5</p>
+    
+    <hr>
+    <h4 class="text-secondary">Padding</h4>
+    <div class="p-1 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="p-2 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="p-3 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="p-4 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="p-5 d-inline-block bg-secondary text-white">Rex</div>
+    <hr>
+
+    <h4 class="text-secondary">Padding Sides</h4>
+    <div class="ps-3 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="pt-3 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="pe-3 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="pb-3 d-inline-block bg-secondary text-white">Rex</div>
+    
+    <hr>
+    <h4 class="text-secondary">Padding Sides</h4>
+    <div class="px-3 d-inline-block bg-secondary text-white">Rex</div>
+    <div class="py-3 d-inline-block bg-secondary text-white">Rex</div>
+    <hr>
+
+</div>
+
+
+<div class="container">
+    <h1>Margin</h1>
+    <h3>p{DIR}-{AMT}</h3>
+    <h3><b>DIR</b></h3>
+    <p>t e b s x y</p>
+    <h3><b>AMT</b></h3>
+    <p>0 1 2 3 4 5 auto</p>
+
+    <hr>
+    <h4 class="text-danger">Margin</h4>
+    <div class="bg-warning d-inline-block"><div class="m-1 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="m-2 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="m-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="m-4 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="m-5 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+
+    <hr>
+    <h4 class="text-secondary">Margin Sides</h4>
+    <div class="bg-warning d-inline-block"><div class="mt-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="me-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="mb-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="ms-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="mx-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+    <div class="bg-warning d-inline-block"><div class="my-3 p-2 d-inline-block bg-secondary text-white">Rex</div></div>
+
+    <hr>
+    <h4 class="text-secondary">Margin Sides</h4>
+    <div class="mt-3 bg-warning w-100"><div style="width:300px;" class="mx-auto p-2 d-block bg-secondary text-white">Rex</div></div>
+
+</div>
+<?php include 'footer.php'; ?>
