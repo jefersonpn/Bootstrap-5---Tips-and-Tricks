0894210e54ac52c48811c34f8bbebbb778d109bc
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-05-18 20:10:32 -0400

    Bootstrap

---
+++ public/icon.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+<h1><i class="bi bi-heart-fill me-2" style="color:red; font-size: 25px;"></i>Font Icons</h1>
+<h1>
+    <svg xmlns="http://www.w3.org/2000/svg" overflow="visible" width="25px" height="25px" class="bi bi-heart-fill me-2" viewBox="0 0 16 16">
+    <path fill="transparent" stroke="red" fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+    </svg>    
+    SVG Icons
+</h1>
+
+<?php include 'footer.php'; ?>
